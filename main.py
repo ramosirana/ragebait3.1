ab1703edbaafacb03553ee924f69d5da3b9abbb1
ramosirana <mighellramhose.17@gmail.com>
2026-01-20 13:03:03 +0800

    Update main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+from js import document
+
+def sub(event):
+
+    regy = document.getElementById("yes")
+    regn = document.getElementById("no")
+    medy = document.getElementById("yes2")
+    medn = document.getElementById("no2")
+    g7 = document.getElementById("g7")
+    if regn and medn:
+        pass
+    else:
+        print('Incomplete')
+        return
